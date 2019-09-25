d080b5370c47c8da0c6f36550f33df7d775d48a6
SunTalk <infernal56727@gmail.com>
2019-09-25 19:44:01 +0800

    update prime.s

--- HW01/prime.s
+++ HW01/prime.s
@@ -1,58 +1,77 @@
+.equ num ,100
+
 .section .rodata
 .align 2
-.str:
-	.string "%d "
-	.text
-	.align 2
-	.global main
-.next:
-	.string "\n"
-	.text
-	.align 2
-	.global main
+
+prime:
+    .space num*4                    ! int prime[100]
+
+.str1:
+    .string "%d "
+    .text
+.str2:
+    .string "\n"
+    .text
+    .global main
 
 main:
+    movi   $r9, 1                   ! r9 = i = 1
+    la     $r8, prime               ! r8 = prime address
+    movi  $r10, 1
 
-movi $r8,#1
-movi $r9,#0
+.set_prime:
 
-.L1:
+    sw    $r10, [$r8+($r9<<2)]      ! prime[i] = 1
+    addi   $r9, $r9, 1              ! i++
+    move   $r7, $r9
+    sltsi  $r7, $r7, num            ! i < 100
+    bnez   $r7, .set_prime
 
-	addi $r8,$r8,#1
-	slti $r6,$r8,#100
-	beqz $r6, .end
+    ! end set_prime
 
-	movi $r7, #2
-.L2:
+    movi   $r9, 2                   ! r9 = i = 2
+    movi   $r4, 1                   ! r4 = 1
+    movi   $r6, 0                   ! int k = 0
 
-	slt $r6,$r7,$r8
-	beqz $r6, .print
+.Loop1:
+    move   $r7, $r9
+    sltsi  $r7, $r7, num            ! i < 100
+    beqz   $r7, .finish             
+    lw    $r10, [$r8+($r9<<2)]      ! r10 = prime[i]
+    movi   $r4, 1
+    beq   $r10, $r4, .Loop2         ! if( prime[i] )
+    addi   $r9, $r9, 1              ! i++
+    b         .Loop1
 
-	divsr $r2,$r1,$r8,$r7
-	beqz $r1 ,.L1
+.Loop2:
+    move   $r3, $r9                 ! int j = i
+    add    $r3, $r3, $r9            ! j = j + i
+    movi   $r5, 0
 
-	addi $r7,$r7,#1
+.sieve:
+    move   $r7, $r3
+    sltsi  $r7, $r7, num            ! j < 100
+    beqz   $r7, .print_prime           
+    sw     $r5, [$r8+($r3<<2)]      ! prime[j] = 0
+    add    $r3, $r3, $r9            ! j = j + i
+    b   .sieve
 
-	j .L2
+.print_prime:
+    move   $r1, $r9
+    la     $r0, .str1
+    bal printf                      ! printf("%d ",i)
+    addi   $r9, $r9, 1              ! i++
+    addi   $r6, $r6, 1              ! k++
+    movi   $r4, 10
+    divsr  $r1, $r2, $r6, $r4       ! if( k%10 == 0 )
+    beqz   $r2, .print_enter
+    b   .Loop1
 
-.print:
+.print_enter:
+    la     $r0, .str2
+    bal printf                      ! printf("\n")
+    b   .Loop1
 
-	la $r0, .str
-	move $r1,$r8
-	bal printf
-
-	addi $r9,$r9,1
-	movi $r5,#10
-	divsr $r2, $r1,$r9,$r5
-	beqz $r1, .enter
-
-	j .L1
-
-.enter:
-
-	la $r0,.next
-	bal printf
-	j .L1
-
-.end:
-	ret
+.finish:
+    ret
+    .end
